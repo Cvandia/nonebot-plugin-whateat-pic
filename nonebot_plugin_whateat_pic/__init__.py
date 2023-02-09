c84b3897b8ea2909a493a9bf91587ae15b7bb42f
divandia <106718176+Cvandia@users.noreply.github.com>
2023-02-09 21:58:03 +0800

    更新```正则匹配```机制，增加机器人名字
    添加更多的美食图片

--- nonebot_plugin_whateat_pic/__init__.py
+++ nonebot_plugin_whateat_pic/__init__.py
@@ -2,10 +2,11 @@ from nonebot.adapters.onebot.v11 import MessageSegment
 from nonebot.plugin import on_regex
 from pathlib import Path
 import os
+import nonebot
 import random
 
-what_eat=on_regex(r"^([今天|明天]?[早上|晚上|中午|夜宵])+吃什么",priority=5)
-what_drink=on_regex(r"^([今天|明天]?[早上|晚上|中午|夜宵])+喝什么",priority=5)
+what_eat=on_regex(r"^((今天|明天)|(今天|明天)?(早上|晚上|中午|夜宵))吃什么$",priority=5)
+what_drink=on_regex(r"^((今天|明天)|(今天|明天)?(早上|晚上|中午|夜宵))喝什么$",priority=5)
 
 #今天吃什么路劲
 img_eat_path = Path(os.path.join(os.path.dirname(__file__), "eat_pic"))
@@ -15,12 +16,16 @@ all_file_eat_name = os.listdir(str(img_eat_path))
 img_drink_path = Path(os.path.join(os.path.dirname(__file__), "drink_pic"))
 all_file_drink_name= os.listdir(str(img_drink_path))
 
+#载入bot名字
+Bot_NICKNAME = list(nonebot.get_driver().config.nickname)
+Bot_NICKNAME = Bot_NICKNAME[0] if Bot_NICKNAME else "脑积水"
+
 @what_drink.handle()
 async def wtd():
     img_name=random.choice(all_file_drink_name)
     img=img_drink_path / img_name
     msg=(
-        f"建议你喝: \n⭐{img.stem}⭐\n"
+        f"{Bot_NICKNAME}建议你喝: \n⭐{img.stem}⭐\n"
         +MessageSegment.image(img)
     )
     try:
@@ -36,7 +41,7 @@ async def wte():
     img_name = random.choice(all_file_eat_name)
     img = img_eat_path / img_name
     msg=(
-        f"建议你吃: \n⭐{img.stem}⭐\n"
+        f"{Bot_NICKNAME}建议你吃: \n⭐{img.stem}⭐\n"
         +MessageSegment.image(img)
     )
     try:
