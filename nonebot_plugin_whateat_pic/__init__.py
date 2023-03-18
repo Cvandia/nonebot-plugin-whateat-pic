38d66045759c185cbd6e091498248b682ac419e9
divandia <106718176+Cvandia@users.noreply.github.com>
2023-03-18 13:35:28 +0800

    ```添加菜品```,懒，后续再写```删除菜品```

--- nonebot_plugin_whateat_pic/__init__.py
+++ nonebot_plugin_whateat_pic/__init__.py
@@ -1,4 +1,7 @@
 from nonebot.adapters.onebot.v11 import MessageSegment,MessageEvent,Bot,Message,GroupMessageEvent
+from nonebot.adapters.onebot.v11.permission import GROUP_ADMIN, GROUP_OWNER
+from nonebot.permission import SUPERUSER
+from nonebot.adapters.onebot.v11.helpers import extract_image_urls
 from nonebot.exception import ActionFailed
 from nonebot.plugin import on_regex
 from nonebot.matcher import Matcher
@@ -9,12 +12,14 @@ from pathlib import Path
 import os
 import re
 import nonebot
+import requests
 import random, base64
 
 what_eat = on_regex(r"^(/)?[今|明|后]?[天|日]?(早|中|晚)?(上|午|餐|饭|夜宵|宵夜)吃(什么|啥|点啥)$",priority=5)
 what_drink = on_regex(r"^(/)?[今|明|后]?[天|日]?(早|中|晚)?(上|午|餐|饭|夜宵|宵夜)喝(什么|啥|点啥)$",priority=5)
 view_all_dishes = on_regex(r"^(/)?查[看|寻]?全部(菜[单|品]|饮[料|品])$",priority=5)
 view_dish = on_regex(r"^(/)?查[看|寻]?(菜[单|品]|饮[料|品])[\s]?(.*)?")
+add_dish = on_regex(r"^(/)?添[加]?(菜[品|单]|饮[品|料])[\s]?(.*)?", priority=99, permission= GROUP_ADMIN|GROUP_OWNER|SUPERUSER)
 
 #今天吃什么路径
 img_eat_path = Path(os.path.join(os.path.dirname(__file__), "eat_pic"))
@@ -27,6 +32,42 @@ all_file_drink_name= os.listdir(str(img_drink_path))
 #载入bot名字
 Bot_NICKNAME = list(nonebot.get_driver().config.nickname)
 Bot_NICKNAME = Bot_NICKNAME[0] if Bot_NICKNAME else "脑积水"
+
+
+
+@add_dish.handle()
+async def got_dish_name(matcher:Matcher,state:T_State):
+    args = list(state['_matched_groups'])
+    state['type'] = args[1]
+    if args[2]:
+        matcher.set_arg("dish_name",args[2])
+        
+@add_dish.got("dish_name",prompt="⭐请发送名字\n发送“取消”可取消添加")
+async def got(state:T_State,dish_name:Message = Arg()):        
+    state['name'] = str(dish_name)
+    if str(dish_name) == "取消":
+        await add_dish.finish("已取消")
+            
+@add_dish.got("img",prompt="⭐图片也发给我吧\n发送“取消”可取消添加")
+async def handle(state:T_State, img:Message = Arg()):
+    if str(img)== "取消":
+        await add_dish.finish("已取消")
+    img_url = extract_image_urls(img)
+    if not img_url:
+        await add_dish.finish("没有找到图片(╯▔皿▔)╯，请稍后重试",at_sender = True)
+        
+        
+    if state['type'] in ['菜品','菜单']:
+        path = img_eat_path
+    elif state['type'] in ['饮料','饮品']:
+        path = img_drink_path
+        
+        
+    dish_img = requests.get(url = img_url[0])
+    with open(path / str(state['name']+".jpg"),"wb") as f:
+        f.write(dish_img.content)
+    await add_dish.finish(f"成功添加{state['type']}:{state['name']}")
+
 
 
 @view_dish.handle()
